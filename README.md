095d471c4c74686c16f66ecc2ddf0eade2b19370
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-07-27 18:42:17 -0300

    Commit inicial.

--- README.md
+++ README.md
@@ -2,3 +2,40 @@ ffthumb
 =======
 
 A simple thumb creator videos in nodejs
+
+Install
+-------
+
+on OS X:
+
+    brew install ffmpegthumbnailer
+
+on Ubuntu:
+
+    sudo apt-get install ffmpegthumbnailer
+
+then:
+
+    npm install ffthumb
+
+
+How to use
+----------
+
+```
+var fft = require('./ffthumb');
+
+var ff = new fft.obj();
+
+function on_error(error) {
+  console.log(error);
+}
+
+ff.create('Despicable_Me_2.avi').
+    output('thumb.png').
+    size('100').
+    error(on_error).
+    done();
+```
+
+![](thumb.png)
---
+++ ffthumb.js
@@ -0,0 +1,50 @@
+var exec = require('child_process').exec;
+
+var obj = function() {
+  this.command = "ffmpegthumbnailer";
+  this.input_filename = undefined;
+  this.output_filename = undefined;
+  this.error_callback = undefined;
+  this.thumb_size = '0';
+}
+
+obj.prototype.create = function(video) {
+  this.input_filename = video;
+  return this;
+}
+
+obj.prototype.size = function(size) {
+  this.thumb_size = size;
+  return this;
+}
+
+obj.prototype.output = function(video) {
+  this.output_filename = video;
+  return this;
+}
+
+obj.prototype.error = function(error_callback) {
+  this.error_callback = error_callback;
+  return this;
+}
+
+obj.prototype.done = function() {
+  var self = this;
+  var commandline = [this.command,
+                    '-i', '"' + this.input_filename + '"',
+                    '-o', '"' + this.output_filename + '"',
+                    '-s' + this.thumb_size]
+
+  exec(commandline.join(' '), function(error, stdout, stderr) {
+
+    if (error !== null) {
+      if (self.error_callback)
+        self.error_callback.call(this, "" + error);
+    }
+
+  });
+}
+
+module.exports = {
+  obj: obj
+}
